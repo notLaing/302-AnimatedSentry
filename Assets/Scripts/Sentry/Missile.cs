804a099d3867a239246da7551aa5bc6ec1ad7c7b
Justin Lai <laij3@ferris.edu>
2022-02-28 00:23:06 -0500

    Audio, spawning projectiles (sentry)

--- Assets/Scripts/Sentry/Missile.cs
+++ Assets/Scripts/Sentry/Missile.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //move relative to up orientation
+        transform.position += transform.up * speed * Time.deltaTime;
+        transform.RotateAround(transform.position, transform.up, 120f * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //
     }
 }
--- Assets/Scripts/Sentry/Shooter.cs
+++ Assets/Scripts/Sentry/Shooter.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    public GameObject projectile;
+    public Transform projectileSpawner;
+    EnemyController sentryScript;
+    PointAt pointScript;
+    public float shootTime = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointScript = GetComponent<PointAt>();
+
+        Transform xform = gameObject.transform;
+        do
+        {
+            if (GetComponentInParent<EnemyController>() != null)
+            {
+                sentryScript = GetComponentInParent<EnemyController>();
+                break;
+            }
+            xform = xform.parent;
+        } while (xform != null);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float distToPlayer = Vector3.SqrMagnitude(pointScript.target.position - transform.position);
+
+        //time ticks if < 10 units away
+        if (distToPlayer <= 100f)
+        {
+            shootTime -= Time.fixedDeltaTime;
+            if (shootTime <= 0f)
+            {
+                //shoot
+                Shoot();
+
+                //reset time
+                shootTime += 5f;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(projectile, projectileSpawner.position, projectileSpawner.rotation);
     }
 }
